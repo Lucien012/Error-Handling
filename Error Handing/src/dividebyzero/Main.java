af0344af82de0a5c00bf15ff1261273f6dd451c9
Lucien012 <97016647+Lucien012@users.noreply.github.com>
2024-03-19 13:20:28 +0800

    try/catch block added

--- Error Handing/src/dividebyzero/Main.java
+++ Error Handing/src/dividebyzero/Main.java
@@ -6,7 +6,12 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(3/0);
+        try {
+            System.out.println(3/0);
+        }catch (ArithmeticException e){
+            System.out.printf("Caught runtime exception = %s\n", e.getMessage());
+        }
+
     }
 
 }
